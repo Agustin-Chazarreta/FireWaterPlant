83cd6020b2c5093400cf28002086f7fbad167945
Agustin-Chazarreta <alchazarreta0@gmail.com>
2021-05-10 18:23:32 -0300

    new removed unnecessary things

--- juego.js
+++ juego.js
@@ -1,24 +1,14 @@
-var random = document.getElementById("random");
-
-const scoreBoard = document.getElementById("scoreBoard");
 const buttons = document.getElementById("buttons");
 const scorePlayer1 = document.getElementById("scoreP1");
 const scorePlayer2 = document.getElementById("scoreCP");
-var button2 = document.getElementById("button2");
-const buttonWater = document.getElementById("buttonWater");
-const buttonFire = document.getElementById("buttonFire");
-const buttonPlant = document.getElementById("buttonPlant");
 const imageCP = document.getElementById("imageCP");
 const imagePlayer = document.getElementById("imagePlayer");
 buttons.addEventListener("click", function (e) {
   var elements = [{ label: "Water" }, { label: "Fire" }, { label: "Plant" }];
-  e.stopPropagation();
   var numMax = elements.length;
   var randomNumber = Math.floor(Math.random() * numMax);
   randomElement = elements[randomNumber].label;
 
-  // imageCP.src = `/imagenes/${randomElement.toLowerCase()}.png`;
-  // randomButton=elements[randomNumber].button
   if (e.target.id === "Water" || e.target.id === "buttonWater") {
     imagePlayer.src = "/imagenes/water.png";
     imageCP.src = `/imagenes/${randomElement.toLowerCase()}.png`;
@@ -32,7 +22,6 @@ buttons.addEventListener("click", function (e) {
     if (randomElement === "Water") {
     }
   }
-
   if (e.target.id === "Fire" || e.target.id === "buttonFire") {
     imagePlayer.src = "/imagenes/fire.png";
     imageCP.src = `/imagenes/${randomElement.toLowerCase()}.png`;
