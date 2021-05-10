69ece03e05f442b6f918fb388018fa68406afd45
Agustin-Chazarreta <alchazarreta0@gmail.com>
2021-05-10 17:26:51 -0300

    New

--- juego.js
+++ juego.js
@@ -1,5 +1,5 @@
 var random = document.getElementById("random");
-const spaceRandom = document.getElementById("spaceRandom");
+
 const scoreBoard = document.getElementById("scoreBoard");
 const buttons = document.getElementById("buttons");
 const scorePlayer1 = document.getElementById("scoreP1");
@@ -12,59 +12,53 @@ const imageCP = document.getElementById("imageCP");
 const imagePlayer = document.getElementById("imagePlayer");
 buttons.addEventListener("click", function (e) {
   var elements = [{ label: "Water" }, { label: "Fire" }, { label: "Plant" }];
-
+  e.stopPropagation();
   var numMax = elements.length;
   var randomNumber = Math.floor(Math.random() * numMax);
   randomElement = elements[randomNumber].label;
 
-  imageCP.src = `/imagenes/${randomElement.toLowerCase()}.png`;
+  // imageCP.src = `/imagenes/${randomElement.toLowerCase()}.png`;
   // randomButton=elements[randomNumber].button
   if (e.target.id === "Water" || e.target.id === "buttonWater") {
     imagePlayer.src = "/imagenes/water.png";
+    imageCP.src = `/imagenes/${randomElement.toLowerCase()}.png`;
     if (randomElement === "Fire") {
-      spaceRandom.innerHTML = `Computer choose ${randomElement}, you win. `;
       scorePlayer1.innerHTML++;
     }
     if (randomElement === "Plant") {
-      spaceRandom.innerHTML = `Computer choose ${randomElement}, you loose. `;
       scorePlayer2.innerHTML++;
     }
 
     if (randomElement === "Water") {
-      spaceRandom.innerHTML = `Computer choose ${randomElement}, it's a tie. `;
     }
   }
 
   if (e.target.id === "Fire" || e.target.id === "buttonFire") {
     imagePlayer.src = "/imagenes/fire.png";
+    imageCP.src = `/imagenes/${randomElement.toLowerCase()}.png`;
     if (randomElement === "Plant") {
-      spaceRandom.innerHTML = `Computer choose ${randomElement}, you win. `;
       scorePlayer1.innerHTML++;
     }
 
     if (randomElement === "Water") {
-      spaceRandom.innerHTML = `Computer choose ${randomElement}, you loose.  `;
       scorePlayer2.innerHTML++;
     }
 
     if (randomElement === "Fire") {
-      spaceRandom.innerHTML = `Computer choose ${randomElement},  it's a tie. `;
     }
   }
 
   if (e.target.id === "Plant" || e.target.id === "buttonPlant") {
     imagePlayer.src = "/imagenes/plant.png";
+    imageCP.src = `/imagenes/${randomElement.toLowerCase()}.png`;
     if (randomElement === "Water") {
-      spaceRandom.innerHTML = `Computer choose ${randomElement}, you win. `;
       scorePlayer1.innerHTML++;
     }
     if (randomElement === "Fire") {
-      spaceRandom.innerHTML = `Computer choose ${randomElement}, you loose. `;
       scorePlayer2.innerHTML++;
     }
 
     if (randomElement === "Plant") {
-      spaceRandom.innerHTML = `Computer choose ${randomElement}, it's a tie. `;
     }
   }
 });
